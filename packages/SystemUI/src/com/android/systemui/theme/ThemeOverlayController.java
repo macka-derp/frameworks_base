91ee3d42cd4e8f99317713b6ef6638672c0ce1bb
Pranav Vashi <neobuddy89@gmail.com>
2022-09-17 12:20:38 +0530

    ThemeOverlayController: Remove repeated monet check
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -512,9 +512,6 @@ public class ThemeOverlayController implements CoreStartable, Dumpable, TunerSer
                     }
                 },
                 UserHandle.USER_ALL);
-        if (!mIsMonetEnabled) {
-            return;
-        }
 
         mUserTracker.addCallback(mUserTrackerCallback, mMainExecutor);
 
